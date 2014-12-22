f26c1657ef4437e680a8c77c855856a1dfa9f079
Matt Merrifield <rmatthewmerrifield@gmail.com>
2014-12-22 12:32:06 -0800

    Re-configured header dictionary, and set up logging stuff

--- devices.py
+++ devices.py
@@ -2,24 +2,31 @@ import telnetlib
 import logging
 import re
 import datetime
-
+from collections import OrderedDict
 __author__ = 'MMerrifield'
+
+class Column(str):
+    def __new__(cls, name, units):
+        self = str.__new__(cls, name)
+        self.units = units
+        return self
+
+    def __init__(self, name, units):
+        super(Column, self).__init__(name)
 
 class iBTHX(object):
 
-
     def __init__(self, hostname=None, port=None):
-        self.host = hostname or 'http://192.168.102.49'
+        self.host = hostname or '192.168.102.49'
         self.port = port or 2000
 
-        self.telnet = telnetlib.Telnet()
-        self.telnet.open(hostname, port)
-
-        self.column_headers = ['Ambient Temperature (C)', 'Ambient Pressure (mba)', 'Ambient Humidity (%RH)', 'Dew Point (F)']
-        self.packet_headers = ['T', 'P', 'H', 'D']
-        self.unit_options = ['C', 'mba', '%', 'F']
-
-        self.header_mapping = dict(zip(self.packet_headers, self.column_headers))
+        self.telnet = telnetlib.Telnet(self.host, self.port)
+        self.columns = OrderedDict([
+            ("T", Column("Temperature", "C")),
+            ("P", Column("Ambient Pressure", "mba")),
+            ("H", Column("Humidity", "%")),
+            ("D", Column("Dew Point", "F")),
+        ])
 
 
     def parse_data(self, item):
@@ -31,18 +38,18 @@ class iBTHX(object):
 
         # The first character is going to be one of the pre-defined header characters, as is the last units string
         # Construct a regex looking for (header)(data)(units) that separates them out.
-        type_choices = '|'.join(self.packet_headers)
-        unit_choices = '|'.join(self.unit_options)
+        type_choices = '|'.join([abbr for abbr in self.columns.keys()])
+        unit_choices = '|'.join([col.units for col in self.columns.values()])
 
         # parse out the data
-        pattern = r"(" + type_choices + r")([\d.]+)(" + unit_choices + r")"
+        pattern = r"({type})([\d.]+)({units})".format(type=type_choices, units=unit_choices)
         parsed_item = re.search(pattern, item)
         type = parsed_item.group(1)
-        data = parsed_item.group(2)
+        data = float(parsed_item.group(2))
         units = parsed_item.group(3)
 
         # Return a tuple of (header)(data)(units)
-        return (type, data, units)
+        return type, data, units
 
     def read_line(self):
         """
@@ -50,37 +57,25 @@ class iBTHX(object):
         """
         data = {}
         timeout_counter = 0
-        timeout_limit = len(self.column_headers) + 2
+        timeout_limit = len(self.columns) + 2
         # Note: if we get too many repeat data items, something might be wrong.
 
-        while len(data) < len(self.column_headers) or timeout_counter > timeout_limit:
+        while len(data) < len(self.columns) or timeout_counter > timeout_limit:
             item = self.telnet.read_until('\r')
             head, data_item, units = self.parse_data(item)
-            column_heading = self.header_mapping[head]
-            data[column_heading] = data_item
+            data[head] = data_item
             timeout_counter += 1
-        return data
-
-    def listen(self):
-        """
-        Constantly listens over the telnet port. Prints a stream of timestamped data.
-        """
-        while True:
-            line = self.read_line()
-            time = datetime.datetime.now()
-            print time, line
-
-
-class DataLogger(object):
-    def __init__(self, device, log_file=None):
-        self.device = device
-        self.logfile = log_file
-
-
-
+        returndata = OrderedDict([(column, data[abbr]) for abbr, column in self.columns.iteritems()])
+        return returndata
 
 
 if __name__ == '__main__':
     ibthx = iBTHX()
-    logger = DataLogger(ibthx)
-    ibthx.listen()
+    log = logging.getLogger(__name__)
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+
+    while True:
+        line = ibthx.read_line()
+        time = datetime.datetime.now().isoformat()
+        msg = "{{{}: {}}}".format(str(time), line)
+        log.info(msg)
--- main.py
+++ main.py
@@ -1,2 +1,50 @@
+from devices import iBTHX
+import logging
+from datetime import datetime
+from logging import handlers
+
 __author__ = 'MMerrifield'
+
+class TimedRotatingFileHanderWithHeader(handlers.TimedRotatingFileHandler):
+    def __init__(self, logger=None, header=None, *args, **kwargs):
+        super(TimedRotatingFileHanderWithHeader, self).__init__(*args, **kwargs)
+        self.header = header
+        self.logger = logger
+
+    def doRollover(self):
+        super(TimedRotatingFileHanderWithHeader, self).doRollover()
+        if self.header is not None:
+
+            # Kind of a horrible hack, actually.
+            # Makes sure there's a header on top of every log file.
+            self.logger.info(self.header)
+
+
+
+device = iBTHX()
+header = " | ".join(["{}".format(col, col.units) for col in device.columns.values()])
+
+# Logging Setup
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+format = logging.Formatter('[%(asctime)s] %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+# Log file
+rotating_logfile = TimedRotatingFileHanderWithHeader(logger=log, header=header, filename="iBTHX-humidity.log", when="D")
+rotating_logfile.setFormatter(format)
+rotating_logfile.setLevel(logging.DEBUG)
+log.addHandler(rotating_logfile)
+# Console
+console = logging.StreamHandler()
+console.setFormatter(format)
+console.setLevel(logging.DEBUG)
+log.addHandler(console)
+
+# Main program
+log.info(header)
+while True:
+    data = device.read_line()
+    msg = " | ".join(["{} {}".format(v, k.units).rjust(len(k)) for k, v in data.iteritems()])
+    log.info(msg)
+
+
 
